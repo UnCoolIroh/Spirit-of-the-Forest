f1c2052e57cb3ae4f326dccc6f79908c14783045
tacobellgaming <jared.c.levenson@gmail.com>
2024-12-03 04:37:56 -0500

    Skeleton for random room generation but we'll have to design to have more than one type of room.

---
+++ Assets/Scripts/NewLevels.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewLevels : MonoBehaviour
+{
+    public GameObject advanceButton;
+    public int sceneBuildIndex;
+    //AudioManager audioManager;
+    void Start()
+    {
+        //audioManager = GameObject.FindGameObjectWithTag("audio").GetComponent<AudioManager>();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            //if (sceneBuildIndex == 3)
+            //{
+            //    audioManager.playSFX(audioManager.door);
+            //    audioManager.playMusic();
+            //}
+            //else if (sceneBuildIndex == 4)
+            //{
+            //    audioManager.playSFX(audioManager.hit);
+            //    audioManager.pause();
+            //}
+            //else
+            //{
+            //    audioManager.playSFX(audioManager.door);
+            //}
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+    }
+
+    public void OnClick()
+    {
+        //audioManager.playSFX(audioManager.buttonPress);
+        //if (sceneBuildIndex == 3)
+        //{
+        //    audioManager.unpause();
+        //}
+        SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+    }
+}
